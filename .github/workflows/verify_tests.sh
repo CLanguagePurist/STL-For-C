b6dcb5288077bf5ff2d2eeaf74e665776c0d7954
CLanguagePurist <107034654+CLanguagePurist@users.noreply.github.com>
2022-06-07 21:59:14 -0600

    Delete verify_tests.sh

--- .github/workflows/verify_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-declare status="FAIL"
-
-if [[ -n $(grep 'Fail:               0' ./build/meson-logs/testlog.txt) ]] && \
-   [[ -n $(grep 'Timeout:            0' ./build/meson-logs/testlog.txt) ]] && \
-   [[ -n $(grep 'Unexpected Pass:    0' ./build/meson-logs/testlog.txt) ]]; then status="PASS"; fi
-
-echo $status
